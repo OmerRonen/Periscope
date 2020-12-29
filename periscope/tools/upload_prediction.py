9e645f10c142071649767d6e87bb4e526271d05a
OmerRonen <omerronen10@gmail.com>
2020-12-29 19:39:55 +0200

    family creator

--- periscope/tools/upload_prediction.py
+++ periscope/tools/upload_prediction.py
@@ -8,6 +8,8 @@ from argparse import ArgumentParser
 
 from periscope.analysis.analyzer import get_model_predictions
 from periscope.net.contact_map import get_model_by_name
+from periscope.utils.constants import PATHS
+from periscope.utils.drive import upload_folder
 from periscope.utils.protein import Protein
 from periscope.utils.utils import check_path, pkl_save
 
@@ -28,18 +30,21 @@ def main():
     model_name = args.model
     proteins = args.proteins
     outfolder = args.outfolder
-    check_path(outfolder)
+    outfolder_full = os.path.join(PATHS.periscope, outfolder)
+    check_path(outfolder_full)
     model = get_model_by_name(model_name)
     predictions = get_model_predictions(model, proteins=proteins)
     logits = predictions['logits']
     weights = predictions['weights']
 
     for protein in proteins:
-        outfolder_p = os.path.join(outfolder, protein)
+        outfolder_p = os.path.join(outfolder_full, protein)
         sequence = list(Protein(protein[0:4], protein[4]).str_seq)
         logits_df = pd.DataFrame(np.squeeze(logits[protein]), columns=sequence, index=sequence)
         logits_df.to_csv(os.path.join(outfolder_p, 'logits.csv'))
         pkl_save(os.path.join(outfolder_p, 'weights.pkl'), weights[protein])
+
+    upload_folder(os.path.join(PATHS.periscope, outfolder), outfolder)
 
 
 if __name__ == '__main__':
