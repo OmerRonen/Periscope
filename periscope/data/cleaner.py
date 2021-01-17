ba99bc2f8b34639ad2458dcdf7ee3c99b0b8098f
OmerRonen <omerronen10@gmail.com>
2021-01-17 15:48:01 -0800

    fix

--- periscope/data/cleaner.py
+++ periscope/data/cleaner.py
@@ -47,6 +47,7 @@ def clean_hhblits():
     test = set(DATASETS_FULL.pfam) | set(DATASETS_FULL.membrane) | set(DATASETS_FULL.cameo) | set(DATASETS_FULL.cameo41)
     all_targets = train | test
     for t in all_targets:
+        shutil.rmtree(os.path.join(get_target_path(t), 'hhblits'))
         t_path = os.path.join(get_target_path(t), 'hhblits_new')
         a3m_file = os.path.join(t_path, f'{t}.a3m')
         a2m_file = os.path.join(t_path, f'{t}.a2m')
