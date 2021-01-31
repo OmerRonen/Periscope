c6e10595fda0f3f122fb9134c188956c22d81bae
OmerRonen <omerronen10@gmail.com>
2021-01-30 19:46:45 -0800

    fix tm output

--- periscope/tools/generate_modeller_data.py
+++ periscope/tools/generate_modeller_data.py
@@ -7,7 +7,7 @@ from ..utils.constants import DATASETS, N_REFS, DATASETS_FULL
 def parse_args():
     parser = ArgumentParser(description="Generates modeller data")
     parser.add_argument('-d', '--dataset', type=str)
-    parser.add_argument('-m', '--model_name', type=str)
+    parser.add_argument('-m', '--model_name', type=str, default=None)
 
     return parser.parse_args()
 
@@ -15,12 +15,16 @@ def parse_args():
 def main():
     args = parse_args()
     dataset = getattr(DATASETS_FULL, args.dataset)
-    model = get_model_by_name(args.model_name)
+    if args.model_name is not None:
+        model = get_model_by_name(args.model_name)
+    else:
+        model = None
     for target in dataset:
         try:
             DataCreator(target, N_REFS).run_modeller_templates(model=model)
         except FileNotFoundError:
             pass
+
 
 if __name__ == '__main__':
     main()
